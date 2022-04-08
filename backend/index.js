3aa98ce61c4a080de4938ff40ded1208de85a041
DESKTOP-SVTJC06\raicley_santana <raicleysantana1@gmail.com>
2022-04-07 23:06:04 -0400

    Ajustes na conexão do banco de dados Adicionado a biblioteca dontenv

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,10 @@ const cors = require("cors");
 const routes = require("./src/routes");
 const app = express();
 
+require('dotenv').config();
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-
-console.log(process.env.PORT || 3333);
 app.listen(process.env.PORT || 3333);
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,14 +1,23 @@
 const Sequelize = require("sequelize");
+require("dotenv/config");
 
-const sequelize = new Sequelize("loja2", "root", "", {
-    host: "localhost",
+const sequelize = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+  {
+    host: process.env.HOST,
     dialect: "mysql",
-});
+  }
+);
 
-/*sequelize.authenticate().then(() => {
+sequelize
+  .authenticate()
+  .then(() => {
     console.log("Conexão com o banco de dados realizado com sucesso!");
-}).catch((error) => {
-    console.log("Error de conexão com banco de dados: " + error)
-})*/
+  })
+  .catch((error) => {
+    console.log("Error de conexão com banco de dados: " + error);
+  });
 
 module.exports = sequelize;
